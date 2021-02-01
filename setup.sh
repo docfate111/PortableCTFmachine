1784799b69151a03a75af85c985b71862ec6fe56
docfate111 <tdwilliamsiv@gmail.com>
2021-02-01 11:42:11 -0800

    fix bug

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker build -f Dockerfile -t ctf:latest 
+docker build . -f Dockerfile -t ctf:latest 
 docker run -it --rm --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v $(pwd):/home ctf:latest /bin/zsh
